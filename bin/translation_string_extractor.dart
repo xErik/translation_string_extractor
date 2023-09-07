a7289f3a3f88db26f0af3f474e43c9634cd41559
xErik <thecoffeejunkie@hotmail.de>
2023-09-07 14:07:30 +0200

    c

--- bin/translation_string_extractor.dart
+++ bin/translation_string_extractor.dart
@@ -4,12 +4,15 @@ import 'package:translation_string_extractor/translation_string_extractor.dart';
 void main(List<String> arguments) {
   final argParser = ArgParser()
     ..addOption('dir', abbr: 'd', defaultsTo: './lib')
-    ..addOption('output', abbr: 'o', defaultsTo: 'strings.json');
+    ..addOption('output', abbr: 'o', defaultsTo: 'strings.json')
+    ..addOption('verbose', abbr: 'v', hide: true);
 
   final argResults = argParser.parse(arguments);
 
   final directory = argResults['dir'];
   final outputFile = argResults['output'];
+  final isVerbose = argResults['verbose'] != null;
 
-  TranlationStringExtractor.extractStrings(directory, outputFile);
+  TranlationStringExtractor.extractStrings(directory, outputFile,
+      isVerbose: isVerbose);
 }
--- lib/translation_string_extractor.dart
+++ lib/translation_string_extractor.dart
@@ -10,11 +10,12 @@ class TranlationStringExtractor {
   static final RegExp _re4 = RegExp('"""((.|\r|\n)*?)"""[ \t\r\n]*\.tr');
   static final List<RegExp> regexes = [_re1, _re2, _re3, _re4];
 
-  static void extractStrings(String directory, String outputFile) {
-    write(read(directory), outputFile);
+  static void extractStrings(String directory, String outputFile,
+      {bool isVerbose = false}) {
+    write(read(directory, isVerbose: isVerbose), outputFile);
   }
 
-  static Map<String, String> read(String directory) {
+  static Map<String, String> read(String directory, {isVerbose = false}) {
     SplayTreeMap<String, String> map = SplayTreeMap<String, String>();
 
     for (final FileSystemEntity entity in Directory(directory)
@@ -22,6 +23,7 @@ class TranlationStringExtractor {
         .where((element) => element.path.endsWith('.dart'))
         .where((element) => element.path.endsWith('.g.dart') == false)) {
       if (entity is File) {
+        int fileMatchCount = 0;
         final content = File(entity.path).readAsStringSync();
         for (final RegExp regex in regexes) {
           final matches = regex.allMatches(content);
@@ -33,6 +35,9 @@ class TranlationStringExtractor {
               map.putIfAbsent(extract, () => extract);
             }
           }
+        }
+        if (isVerbose) {
+          print(entity.path + '\t' + fileMatchCount.toString() + ' matches');
         }
       }
     }
